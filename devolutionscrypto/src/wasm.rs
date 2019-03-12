11e3a77add70294a4e5eb0e056d647b0f2d5e834
Philippe Dugre <pdugre@devolutions.net>
2019-03-12 11:10:43 -0400

    Fixed up wasm bindings

--- devolutionscrypto/src/wasm.rs
+++ devolutionscrypto/src/wasm.rs
@@ -1,6 +1,9 @@
+use std::convert::TryFrom as _;
+
 use wasm_bindgen::prelude::*;
 
 use super::devocrypto;
+use super::DcDataBlob;
 
 #[wasm_bindgen]
 pub struct KeyPair {
@@ -21,34 +24,38 @@ impl KeyPair {
 
 #[wasm_bindgen]
 pub fn encrypt(data: &[u8], key: &[u8]) -> Vec<u8> {
-    devocrypto::encrypt(&data, &key).unwrap()
+    DcDataBlob::encrypt(&data, &key).unwrap().into()
 }
 
 #[wasm_bindgen]
 pub fn decrypt(data: &[u8], key: &[u8]) -> Vec<u8> {
-    devocrypto::decrypt(&data, &key).unwrap()
+    let data_blob = DcDataBlob::try_from(data).unwrap();
+    data_blob.decrypt(&key).unwrap()
 }
 
 #[wasm_bindgen]
-pub fn hash_password(password: &[u8], niterations: u32) -> Vec<u8> {
-    devocrypto::hash_password(&password, niterations).unwrap()
+pub fn hash_password(password: &[u8], iterations: u32) -> Vec<u8> {
+    DcDataBlob::hash_password(&password, iterations).unwrap().into()
 }
 
 #[wasm_bindgen]
 pub fn verify_password(password: &[u8], hash: &[u8]) -> bool {
-    devocrypto::verify_password(&password, &hash).unwrap()
+    let data_blob = DcDataBlob::try_from(hash).unwrap();
+    data_blob.verify_password(&password).unwrap()
 }
 
 #[wasm_bindgen]
 pub fn generate_key_exchange() -> KeyPair {
-    let (public, private) = devocrypto::generate_key_exchange().unwrap();
-    let pair = KeyPair { public_key: public, private_key: private };
+    let (private, public) = DcDataBlob::generate_key_exchange().unwrap();
+    let pair = KeyPair { private_key: private.into(), public_key: public.into() };
     pair
 }
 
 #[wasm_bindgen]
-pub fn mix_key_exchange(public: &[u8], private: &[u8]) -> Vec<u8> {
-    devocrypto::mix_key_exchange(public, private).unwrap()
+pub fn mix_key_exchange(private: &[u8], public: &[u8]) -> Vec<u8> {
+    let private = DcDataBlob::try_from(private).unwrap();
+    let public = DcDataBlob::try_from(public).unwrap();
+    private.mix_key_exchange(public).unwrap()
 }
 
 #[wasm_bindgen]
@@ -57,6 +64,6 @@ pub fn generate_key(length: usize) -> Vec<u8> {
 }
 
 #[wasm_bindgen]
-pub fn derive_key(key: &[u8], salt: &[u8], niterations: usize, length: usize) -> Vec<u8> {
-    devocrypto::derive_key(key, salt, niterations, length)
+pub fn derive_key(key: &[u8], salt: &[u8], iterations: usize, length: usize) -> Vec<u8> {
+    devocrypto::derive_key(key, salt, iterations, length)
 }
