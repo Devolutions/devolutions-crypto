afc01880dd228b455f1850f6296796574a5f3270
Mathieu Morrissette <mmorrissette@devolutions.net>
2020-03-02 11:04:41 -0500

    add documentation and rename methods for clarity

--- wrappers/csharp/Utils.cs
+++ wrappers/csharp/Utils.cs
@@ -5,6 +5,12 @@ namespace Devolutions.Cryptography
     
     public static partial class Utils
     {
+        /// <summary>
+        /// Validate that the buffer is from the Devolutions Crypto Library.
+        /// </summary>
+        /// <param name="data">The buffer to validate.</param>
+        /// <param name="type">The data type to validate.</param>
+        /// <returns>Returns true if the buffer received matches the data type.</returns>
         public static bool ValidateSignature(byte[] data, DataType type)
         {
             if(data == null)
@@ -26,7 +32,23 @@ namespace Devolutions.Cryptography
             return false;
         }
 
+        /// <summary>
+        /// Converts a string to a UTF8 encoded byte array.
+        /// </summary>
+        /// <param name="data">The string to convert.</param>
+        /// <returns>A UTF8 string in a byte array.</returns>
+        [Obsolete("This method has been deprecated. Use StringToUtf8ByteArray instead.")]
         public static byte[] StringToByteArray(string data)
+        {
+            return StringToUtf8ByteArray(data);
+        }
+
+        /// <summary>
+        /// Converts a string to a UTF8 encoded byte array.
+        /// </summary>
+        /// <param name="data">The string to convert.</param>
+        /// <returns>A UTF8 string in a byte array.</returns>
+        public static byte[] StringToUtf8ByteArray(string data)
         {
             if (data == null)
             {
@@ -36,6 +58,12 @@ namespace Devolutions.Cryptography
             return Encoding.UTF8.GetBytes(data);
         }
 
+        /// <summary>
+        /// Converts a byte array to a base 64 encoded string.
+        /// </summary>
+        /// <param name="bytes">The data to convert.</param>
+        /// <returns>A base 64 string.</returns>
+        [Obsolete("This method has been deprecated. Use EncodeToBase64 instead.")]
         public static string ToBase64String(byte[] bytes)
         {
             if (bytes == null || bytes.Length == 0)
@@ -46,6 +74,11 @@ namespace Devolutions.Cryptography
             return Encode(bytes);
         }
 
+        /// <summary>
+        /// Converts a base 64 string to a byte array.
+        /// </summary>
+        /// <param name="data">The data to convert.</param>
+        /// <returns>A byte array.</returns>
         public static byte[] Base64StringToByteArray(string data)
         {
             if (data == null || data.Length == 0)
@@ -53,10 +86,26 @@ namespace Devolutions.Cryptography
                 return null;
             }
 
-            return Decode(data);
+            return DecodeFromBase64(data);
         }
 
+        /// <summary>
+        /// Encode a byte array to a UTF8 encoded string.
+        /// </summary>
+        /// <param name="data">The data to convert.</param>
+        /// <returns>A UTF8 encoded string.</returns>
+        [Obsolete("This method has been deprecated. Use ByteArrayToUtf8String instead.")]
         public static string ByteArrayToString(byte[] data)
+        {
+            return ByteArrayToUtf8String(data);
+        }
+
+        /// <summary>
+        /// Encode a byte array to a UTF8 encoded string.
+        /// </summary>
+        /// <param name="data">The data to convert.</param>
+        /// <returns>A UTF8 encoded string.</returns>
+        public static string ByteArrayToUtf8String(byte[] data)
         {
             if (data == null || data.Length == 0)
             {
@@ -73,14 +122,30 @@ namespace Devolutions.Cryptography
             }
         }
 
+        /// <summary>
+        /// Converts a base 64 string to a byte array.
+        /// </summary>
+        /// <param name="data">The data to convert.</param>
+        /// <returns>A byte array.</returns>
+        [Obsolete("This method has been deprecated. Use DecodeFromBase64 instead.")]
         public static byte[] Decode(string data)
         {
-            if (data == null || data.Length == 0)
+            return DecodeFromBase64(data);
+        }
+
+        /// <summary>
+        /// Converts a base 64 string to a byte array.
+        /// </summary>
+        /// <param name="base64">The data to convert.</param>
+        /// <returns>A byte array.</returns>
+        public static byte[] DecodeFromBase64(string base64)
+        {
+            if (base64 == null || base64.Length == 0)
             {
                 return null;
             }
 
-            int length = GetDecodedLength(data);
+            int length = GetDecodedLength(base64);
 
             if(length == 0)
             {
@@ -89,7 +154,7 @@ namespace Devolutions.Cryptography
 
             byte[] buffer = new byte[length];
 
-            long decode_res = Native.DecodeNative(data, (UIntPtr)data.Length, buffer, (UIntPtr)buffer.Length);
+            long decode_res = Native.DecodeNative(base64, (UIntPtr)base64.Length, buffer, (UIntPtr)buffer.Length);
 
             if (decode_res == -1)
                 return null;
@@ -97,7 +162,23 @@ namespace Devolutions.Cryptography
             return buffer;
         }
 
+        /// <summary>
+        /// Converts a byte array to a base 64 encoded string.
+        /// </summary>
+        /// <param name="data">The data to convert.</param>
+        /// <returns>A base 64 string.</returns>
+        [Obsolete("This method has been deprecated. Use EncodeToBase64 instead.")]
         public static string Encode(byte[] data)
+        {
+            return EncodeToBase64String(data);
+        }
+
+        /// <summary>
+        /// Converts a byte array to a base 64 encoded string.
+        /// </summary>
+        /// <param name="data">The data to convert.</param>
+        /// <returns>A base 64 string.</returns>
+        public static string EncodeToBase64String(byte[] data)
         {
             if (data == null || data.Length == 0)
             {
@@ -115,9 +196,14 @@ namespace Devolutions.Cryptography
 
             long encode_res = Native.EncodeNative(data, (UIntPtr)data.Length, buffer, (UIntPtr)buffer.Length);
 
-            return ByteArrayToString(buffer);
+            return ByteArrayToUtf8String(buffer);
         }
 
+        /// <summary>
+        /// Calculate the length of the resulting array if the buffer is encoded in base64.
+        /// </summary>
+        /// <param name="buffer">The buffer to calculate the resulting length.</param>
+        /// <returns>The resulting base 64 buffer lentgh.</returns>
         public static int GetEncodedLength(byte[] buffer)
         {
             if(buffer == null)
@@ -128,6 +214,11 @@ namespace Devolutions.Cryptography
             return ((4 * buffer.Length / 3) + 3) & ~3;
         }
 
+        /// <summary>
+        /// Calculate the length of the original buffer if the base 64 string is converted back.
+        /// </summary>
+        /// <param name="base64">The base 64 string to calculate the resulting length.</param>
+        /// <returns>The original buffer length.</returns>
         public static int GetDecodedLength(string base64)
         {
             if (string.IsNullOrEmpty(base64)) 
@@ -153,6 +244,10 @@ namespace Devolutions.Cryptography
             return  result;
         }
 
+        /// <summary>
+        /// Method used to throw the right exception depending on the error code.
+        /// </summary>
+        /// <param name="errorCode">The error code to handle.</param>
         public static void HandleError(long errorCode)
         {
             if (Enum.IsDefined(typeof(NativeError), (int)errorCode))
@@ -165,6 +260,10 @@ namespace Devolutions.Cryptography
             }
         }
 
+        /// <summary>
+        /// Gets the native library version.
+        /// </summary>
+        /// <returns>Returns the native library version string.</returns>
         public static string Version()
         {
             long size = Native.VersionSizeNative();
