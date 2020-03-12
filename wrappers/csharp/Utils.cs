831f4370c460da607d5b23dad629f71fa4f8c193
Claude Bien-Aimé <cbienaime@devolutions.net>
2020-03-12 11:07:03 -0400

    Add unit tests for the shamir secret sharing functions.

--- wrappers/csharp/Utils.cs
+++ wrappers/csharp/Utils.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace Devolutions.Cryptography
 {
     using System;
@@ -371,6 +373,49 @@ namespace Devolutions.Cryptography
             }
 
             return Encoding.UTF8.GetString(versionBytes);
+        }
+
+        public static IntPtr[] GetArrayReferences(ref byte[][] shares, int nbShares, int secretLength)
+        {
+            var uSecretLength = (int)Native.GenerateSharedKeySizeNative((UIntPtr)secretLength);
+
+            GCHandle[] handles = new GCHandle[(int)nbShares];
+            for (int i = 0; i < shares.Length; i++)
+            {
+                handles[i] = GCHandle.Alloc(shares[i], GCHandleType.Pinned);
+            }
+
+            IntPtr[] pointers = new IntPtr[(int)nbShares];
+            for (int i = 0; i < handles.Length; i++)
+            {
+                pointers[i] = handles[i].AddrOfPinnedObject();
+            }
+
+            return pointers;
+        }
+
+        public static IntPtr[] InitializeArray(ref byte[][] shares, int nbShares, int secretLength)
+        {
+            var uSecretLength = (int)Native.GenerateSharedKeySizeNative((UIntPtr)secretLength);
+
+            for (int i = 0; i < nbShares; i++)
+            {
+                shares[i] = new byte[uSecretLength];
+            }
+
+            GCHandle[] handles = new GCHandle[(int)nbShares];
+            for (int i = 0; i < shares.Length; i++)
+            {
+                handles[i] = GCHandle.Alloc(shares[i], GCHandleType.Pinned);
+            }
+
+            IntPtr[] pointers = new IntPtr[(int)nbShares];
+            for (int i = 0; i < handles.Length; i++)
+            {
+                pointers[i] = handles[i].AddrOfPinnedObject();
+            }
+
+            return pointers;
         }
     }
 }
