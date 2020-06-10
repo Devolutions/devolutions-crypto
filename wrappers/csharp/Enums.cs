8f2127158583869a12c2aeb7a03d6b0594f74df4
Claude Bien-Aimé <cbienaime@devolutions.net>
2020-06-10 14:20:53 -0400

    Renommer CipherVersion dans le c# pour CiphertextVersion

---
+++ wrappers/csharp/Enums.cs
@@ -0,0 +1,23 @@
+namespace Devolutions.Cryptography
+{
+    public enum DataType 
+    {
+        Key = 1,
+        Cipher = 2,
+        Hash = 3
+    }
+
+    public enum CipherTextVersion
+    {
+        Latest = 0,
+
+        // Aes256CbcHmacSha256
+        V1 = 1,
+
+        // XChaCha20Poly1305
+        V2 = 2,
+
+        // XChaCha20Poly1305 with DeriveKey length fix
+        V2_5 = 2,
+    }
+}
