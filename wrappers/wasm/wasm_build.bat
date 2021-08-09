477952a080ac0bd661555ae5a691846c4d56a3bf
Philippe Dugre <pdugre@devolutions.net>
2021-08-09 14:54:09 -0400

    moved JS package to the devolutions scope

--- wrappers/wasm/wasm_build.bat
+++ wrappers/wasm/wasm_build.bat
@@ -1,2 +1,1 @@
-wasm-pack build ../../devolutions-crypto --out-dir ../wrappers/wasm/pkg -- --features=wbindgen
-wasm-pack pack
+wasm-pack build ../../devolutions-crypto --out-dir ../wrappers/wasm/pkg --target bundler --scope devolutions -- --features=wbindgen
