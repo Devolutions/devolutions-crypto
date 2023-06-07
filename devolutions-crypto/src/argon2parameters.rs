07860fe0c055571abe0a534345d22473815ae023
Luc Fauvel <lfauvel@devolutions.net>
2023-06-07 09:17:44 -0400

    Added wasm_bindgen skip to pub salt

--- devolutions-crypto/src/argon2parameters.rs
+++ devolutions-crypto/src/argon2parameters.rs
@@ -45,6 +45,7 @@ pub struct Argon2Parameters {
     /// Secret key to sign the hash. Note that this is not serialized.
     secret_key: Vec<u8>,
     /// A 16-bytes salt to use that is generated automatically. Should not be accessed directly.
+    #[wasm_bindgen(skip)]
     pub salt: Vec<u8>,
 }
 
