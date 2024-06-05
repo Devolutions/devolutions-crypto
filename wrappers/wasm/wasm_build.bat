fd91b180fc227ecef6487da78c12ddb0233b4ecc
Mathieu Morrissette <mathieumorrissette@gmail.com>
2024-06-05 08:50:25 -0400

    Add publish steps for web package

--- wrappers/wasm/wasm_build.bat
+++ wrappers/wasm/wasm_build.bat
@@ -1,1 +1,4 @@
-wasm-pack build ../../ --out-dir ./wrappers/wasm/pkg --target bundler --scope devolutions -- --features=wbindgen
+wasm-pack build ../../ --out-dir ./wrappers/wasm/dist/bundler --target bundler --scope devolutions -- --features=wbindgen
+wasm-pack build ../../ --out-dir ./wrappers/wasm/dist/node --target nodejs --scope devolutions -- --features=wbindgen
+wasm-pack build ../../ --out-dir ./wrappers/wasm/dist/web --target web --scope devolutions -- --features=wbindgen
+wasm-pack build ../../ --out-dir ./wrappers/wasm/dist/no-modules --target no-modules --scope devolutions -- --features=wbindgen
--- wrappers/wasm/wasm_build.sh
+++ wrappers/wasm/wasm_build.sh
@@ -4,3 +4,5 @@ wasm-pack build ../../ --out-dir ./wrappers/wasm/dist/bundler --target bundler -
 wasm-pack build ../../ --out-dir ./wrappers/wasm/dist/node --target nodejs --scope devolutions -- --features=wbindgen
 wasm-pack build ../../ --out-dir ./wrappers/wasm/dist/web --target web --scope devolutions -- --features=wbindgen
 wasm-pack build ../../ --out-dir ./wrappers/wasm/dist/no-modules --target no-modules --scope devolutions -- --features=wbindgen
+
+sed -i 's/"@devolutions\/devolutions-crypto"/"@devolutions\/devolutions-crypto-web"/' ./dist/web/package.json
