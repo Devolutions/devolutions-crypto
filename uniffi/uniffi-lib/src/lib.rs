04f6a02c64143d45828194050135bf289cf68505
mathieumorrissette <mmorrissette@gmail.com>
2024-08-08 09:41:19 -0400

    fmt clippy

--- uniffi/uniffi-lib/src/lib.rs
+++ uniffi/uniffi-lib/src/lib.rs
@@ -1,12 +1,9 @@
 use devolutions_crypto::utils::generate_key;
 
 #[uniffi::export]
-fn hello() -> Vec<u8>{
-println!("Generating Key in Rust!");
-generate_key(32)
+fn hello() -> Vec<u8> {
+    println!("Generating Key in Rust!");
+    generate_key(32)
 }
-
-
-
 
 uniffi::setup_scaffolding!();
