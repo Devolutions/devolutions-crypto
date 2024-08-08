60a4022bb14a8c23ede3c81c494e8310bbd11be4
mathieumorrissette <mmorrissette@gmail.com>
2024-08-08 08:23:29 -0400

    improvements swift

--- wrappers/swift/generate.sh
+++ wrappers/swift/generate.sh
@@ -1,7 +1,15 @@
+#!/bin/sh
+# based on https://rhonabwy.com/2023/02/10/creating-an-xcframework
+
+
+XCFRAMEWORK_FOLDER="./output/devolutions-crypto.xcframework"
+LIBNAME="uniffi_lib"
+LIBNAMEBUILD="uniffi-lib"
+
 rm -rf bindings
 rm -rf output
 
-cargo run -p uniffi-bindgen generate --library "../../target/debug/libuniffi_lib.dylib"  --language swift -o bindings --no-format
+cargo run -p uniffi-bindgen generate --library "../../target/debug/lib$LIBNAME.dylib"  --language swift -o bindings --no-format
 
 mkdir ./bindings/mac
 mkdir ./bindings/ios-simulator
@@ -13,37 +21,42 @@ rustup target add aarch64-apple-darwin # Arm based mac
 rustup target add x86_64-apple-darwin # Intel based mac
 
 
-cargo build --release --target=x86_64-apple-ios -p uniffi-lib
-cargo build --release --target=aarch64-apple-ios-sim -p uniffi-lib
-cargo build --release --target=aarch64-apple-ios -p uniffi-lib
-cargo build --release --target=aarch64-apple-darwin -p uniffi-lib
-cargo build --release --target=x86_64-apple-darwin -p uniffi-lib
+cargo build --release --target=x86_64-apple-ios -p "$LIBNAMEBUILD"
+cargo build --release --target=aarch64-apple-ios-sim -p "$LIBNAMEBUILD"
+cargo build --release --target=aarch64-apple-ios -p "$LIBNAMEBUILD"
+cargo build --release --target=aarch64-apple-darwin -p "$LIBNAMEBUILD"
+cargo build --release --target=x86_64-apple-darwin -p "$LIBNAMEBUILD"
 
 
-mv ./bindings/uniffi_libFFI.modulemap ./bindings/module.modulemap
+mv "./bindings/${LIBNAME}FFI.modulemap" ./bindings/module.modulemap
  
 # combine the platforms 
 
 # ios simulator
-lipo ../../target/x86_64-apple-ios/release/libuniffi_lib.a \
-    ../../target/aarch64-apple-ios-sim/release/libuniffi_lib.a \
-    -create -output ./bindings/ios-simulator/libuniffi_lib.a
+lipo "../../target/x86_64-apple-ios/release/lib$LIBNAME.a" \
+    "../../target/aarch64-apple-ios-sim/release/lib$LIBNAME.a" \
+    -create -output "./bindings/ios-simulator/lib$LIBNAME.a"
 
 # mac
-lipo ../../target/x86_64-apple-darwin/release/libuniffi_lib.a \
-    ../../target/aarch64-apple-darwin/release/libuniffi_lib.a \
-    -create -output ./bindings/mac/libuniffi_lib.a
+lipo ../../target/x86_64-apple-darwin/release/lib$LIBNAME.a \
+    ../../target/aarch64-apple-darwin/release/lib$LIBNAME.a \
+    -create -output ./bindings/mac/lib$LIBNAME.a
 
 
 # no need to combine ios
 
 # create the XCFramework
 xcodebuild -create-xcframework \
-            -library ./bindings/ios-simulator/libuniffi_lib.a -headers ./bindings \
-            -library ./bindings/mac/libuniffi_lib.a -headers ./bindings \
-            -library ../../target/aarch64-apple-ios/release/libuniffi_lib.a -headers ./bindings \
+            -library "./bindings/ios-simulator/lib$LIBNAME.a" -headers ./bindings \
+            -library "./bindings/mac/lib$LIBNAME.a" -headers ./bindings \
+            -library "../../target/aarch64-apple-ios/release/lib$LIBNAME.a" -headers ./bindings \
             -output "./output/devolutions-crypto.xcframework"
 
+# Compress XCFramework
+ditto -c -k --sequesterRsrc --keepParent "$XCFRAMEWORK_FOLDER" "$XCFRAMEWORK_FOLDER.zip"
+
+# Compute checksum
+swift package compute-checksum "$XCFRAMEWORK_FOLDER.zip"
 
 
 
