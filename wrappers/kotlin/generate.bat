b5bbedb2addc1559d7fbf1713366c320119aee97
mathieumorrissette <mmorrissette@gmail.com>
2024-11-15 10:29:42 -0500

    ci

--- wrappers/kotlin/generate.bat
+++
@@ -1,9 +0,0 @@
-cd "..\..\uniffi\devolutions-crypto-uniffi\"
-cargo build --release
-
-cd "../uniffi-bindgen"
-cargo run -p uniffi-bindgen generate --library "..\..\target\release\devolutions_crypto_uniffi.dll"  --language kotlin -o ../../wrappers/kotlin/lib/src/main/kotlin --no-format
-
-cd "../../"
-
-copy ".\target\release\devolutions_crypto_uniffi.dll" ".\wrappers\kotlin\lib\src\main\resources\win32-x86-64\"
