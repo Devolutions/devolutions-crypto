ac3c8049688539094e7fbe9bfc5021b988533fb1
Mathieu Morrissette <mathieumorrissette@gmail.com>
2025-01-27 10:15:19 -0500

    use android jna

--- wrappers/kotlin/lib/build.gradle.kts
+++ wrappers/kotlin/lib/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     // This dependency is used internally, and not exposed to consumers on their own compile classpath.
     implementation(libs.guava)
 
-    implementation(libs.jna)
+    implementation("net.java.dev.jna:jna:5.16.0@aar")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
