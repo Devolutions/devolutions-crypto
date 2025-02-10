c0b4a5af005a9e70836a29ad1d197241e3a905ab
Mathieu Morrissette <mathieumorrissette@gmail.com>
2025-02-10 12:02:59 -0500

    test

--- wrappers/kotlin/lib/build.gradle.kts
+++ wrappers/kotlin/lib/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 plugins {
-    kotlin("multiplatform")
+    kotlin("multiplatform") version '2.1.10'
     id("com.android.library") // Required for AAR builds
     id("maven-publish")
 }
